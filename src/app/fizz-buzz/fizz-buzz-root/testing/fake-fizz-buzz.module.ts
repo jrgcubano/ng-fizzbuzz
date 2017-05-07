fea16cf25eb32f621a397db6b71c879788d64988
Jorge Rodríguez Galán <jrogalan@gmail.com>
2017-05-07 03:09:42 +0200

    fix(aot): fixed aot build with a fake module

---
+++ src/app/fizz-buzz/fizz-buzz-root/testing/fake-fizz-buzz.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MockFizzBuzzItemsComponent } from './mock-fizz-buzz-items.component'
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [MockFizzBuzzItemsComponent]
+})
+export class MockFizzBuzzModule {
+}
